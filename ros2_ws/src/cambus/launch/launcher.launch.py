c0365bb939ab25d15e343500dae6f8e87c6b6305
MartinIRizzo <martin.ignacio.rizzo14@gmail.com>
2025-11-29 19:12:04 +0100

    created an actual ros2 package

---
+++ ros2_ws/src/cambus/launch/launcher.launch.py
@@ -0,0 +1,69 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess, TimerAction
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    gazebo_dir = get_package_share_directory('turtlebot3_gazebo')
+
+    robot_path = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'urdf',
+        'turtlebot3_waffle.urdf'
+    )
+    world_path = "/shared/worlds/landmark_world.world"
+
+    x = -2.0
+    y = 2.0
+    z = 0.01
+
+    with open(robot_path, 'r') as f:
+        robot_description = f.read()
+
+    start_gazebo = ExecuteProcess(
+        cmd=['gazebo', world_path, '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'],
+        output='screen'
+    )
+
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robot_description
+        }]
+    )
+
+    spawn_robot = TimerAction(
+        period=60.0,
+        actions=[
+            Node(
+                package='gazebo_ros',
+                executable='spawn_entity.py',
+                name='spawn_turtlebot3',
+                output='screen',
+                arguments=[
+                    '-entity', 'turtlerobot3_waffle',
+                    '-file', os.path.join(gazebo_dir, 'models', 'turtlebot3_waffle', 'model.sdf'),
+                    '-x', str(x),
+                    '-y', str(y),
+                    '-z', str(z),
+                ]
+            )
+        ]
+    )
+
+    start_bus = Node(
+        package='cambus',
+        executable='controller',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        start_gazebo,
+        robot_state_publisher,
+        spawn_robot,
+        start_bus
+    ])
